19576b9fc8f59d6ff73fcd5d691960bab1daf96e
MaximeBrt <maxime.baraton92@gmail.com>
2022-08-27 22:33:27 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,25 +3,26 @@ var popi;
 var lifespan = 200;
 var lifeP;
 var count = 0;
+var target;
 
 function setup() {
   createCanvas(400, 400);
   rocket = new Rocket()
   popi = new Population()
   lifeP = createP()
+  target = createVector(width/2, 50)
 }
 
 function draw() {
   background(0);
-  // rocket.update()
-  // rocket.show()
+  ellipse(target.x, target.y, 16, 16)
   popi.run()
   lifeP.html(count)
-  if (count == popi.popsize) {
-    popi.stop()
-  }
-  else {
-    count ++
+  count++
+  if (count == lifespan) {
+    popi.createNewPop()
+    popi = new Population()
+    count = 0
   }
 }
 
@@ -40,11 +41,19 @@ function Population() {
     })
   }
 
-  this.stop = function() {
+  this.createNewPop = function() {
+    var maxFit = 0;
     this.rockets.forEach((rocket) => {
-      rocket.vel = createVector()
+      rocket.evaluate()
+      if (rocket.fitness > maxFit) {
+        maxFit = rocket.fitness
+      }
+    })
+    this.rockets.forEach((rocket) => {
+      rocket.fitness /= maxFit
     })
   }
+  
 }
 
 function DNA() {
@@ -59,7 +68,8 @@ function Rocket() {
   this.pos = createVector(width/2, height);
   this.vel = createVector();
   this.acc = createVector();
-  this.dna = new DNA()
+  this.dna = new DNA();
+  this.fitness = 0;
 
   this.applyForce = function(force) {
     this.acc.add(force)
@@ -82,5 +92,10 @@ function Rocket() {
     rectMode(CENTER)
     rect(0, 0, 25, 5)
     pop()
+  }
+
+  this.evaluate = function() {
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y)
+    this.fitness = map(d, 0, width, width, 0)
   }
 }
